748642b64ab340b4885f42ef16df01632284498f
NoahKleinschmidt <Noah.kleinschmidt@students.unibe.ch>
2022-02-07 12:22:31 +0100

    change Qupid Reader init

--- qpcr/__init__.py
+++ qpcr/__init__.py
@@ -99,10 +99,10 @@ class _Qupid_Reader(Reader):
         A streamlit UploadedFile object
     """
     def __init__(self, file) -> pd.DataFrame: 
-        super().__init__(file)
         self._src = file
-        self._content = self._src.read().decode()
+        self._content = file.read().decode()
         self._delimiter = ";" if self._is_csv2() else ","
+        super().__init__(file)
         self.read()
 
     def _is_csv2(self):
