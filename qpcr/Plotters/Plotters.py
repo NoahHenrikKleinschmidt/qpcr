aaac55007f29139fbbad999beb62045d27f871d6
NoahKleinschmidt <Noah.kleinschmidt@students.unibe.ch>
2022-02-18 00:54:59 +0100

    interactive preview also sorts by group

--- qpcr/Plotters/Plotters.py
+++ qpcr/Plotters/Plotters.py
@@ -526,6 +526,8 @@ class PreviewResults(Plotter):
             for assay in headings:
                 row, col = Coords.get()
                 tmp_df = data.query(query.format(ref_col = ref_col, q = assay))
+                tmp_df = tmp_df.sort_values("group")
+
                 # now plot a new bar chart 
                 fig.add_trace(
 
