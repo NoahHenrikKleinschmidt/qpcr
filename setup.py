f0bfeeea998cf654f0d57245730521f088fc01bc
Noah Kleinschmidt <noah.kleinschmidt@students.unibe.ch>
2022-07-18 22:48:35 +0200

    update to v4.0.0-alpha

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="qpcr", 
-    version="3.1.5",
+    version="4.0.0-alpha",
     author="Noah H. Kleinschmidt",
     author_email="noah.kleinschmidt@students.unibe.ch",
     description="A module to perform analysis of qPCR data",
