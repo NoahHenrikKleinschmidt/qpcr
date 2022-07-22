8968f3ae4168332fd97e3e9f71b25cbe905db66e
Noah Kleinschmidt <noah.kleinschmidt@students.unibe.ch>
2022-07-22 21:55:51 +0200

    update logo and logo background color

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -80,6 +80,8 @@ html_theme = 'sphinx_rtd_theme'
 
 html_logo = "./qpcr_dark.svg"
 
+html_theme_options = dict( style_nav_header_background = "#00aaffff" ) 
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
