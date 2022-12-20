afb9c14332e9656bda8b6387c888cda5bf39718d
Noah Kleinschmidt <noah.kleinschmidt@students.unibe.ch>
2022-12-20 22:19:24 +0100

    update setup

--- setup.py
+++ setup.py
@@ -4,15 +4,32 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setuptools.setup(
-    name="qpcr", 
+    name="qpcr",
     version="4.1.0",
     author="Noah H. Kleinschmidt",
     author_email="noah.kleinschmidt@students.unibe.ch",
-    description="A package to perform analysis of qPCR data",
+    description="A python package to perform analysis of qPCR data",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/NoahHenrikKleinschmidt/qpcr.git",
-    packages=setuptools.find_packages(),
+    packages=[
+        "qpcr",
+        "qpcr.main",
+        "qpcr.stats",
+        "qpcr.Plotters",
+        "qpcr.Readers",
+        "qpcr.Parsers",
+        "qpcr.Filters",
+    ],
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scipy",
+        "matplotlib",
+        "seaborn",
+        "statsmodels",
+        "plotly",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
